91320fc53d8a16d92643efe32ec9573c73324981
OmarEss3 <155445810+OmarEss3@users.noreply.github.com>
2024-11-30 11:56:53 +0200

    scroll bar with animation at prayer times

--- lib/widgets/build_prayer_details_widget.dart
+++ lib/widgets/build_prayer_details_widget.dart
@@ -3,50 +3,54 @@ import '../constants.dart';
 import '../cubit/praying_cubit/praying_cubit.dart';
 import 'prayer_times_widget.dart';
 
-Widget buildPrayerDetails(BuildContext context, PrayingLoaded state,scrollController) {
-    return Column(
-      children: [
-        Container(
-          width: MediaQuery.of(context).size.width * 0.85,
-          height: MediaQuery.of(context).size.height * 0.12,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(12),
-            color: Colors.white,
-          ),
-          padding: const EdgeInsets.all(10),
-          child: Row(
-            children: [
-              Image.asset(
-                "assets/images/next_prayer.png",
-                height: 50,
-              ),
-              SizedBox(width: MediaQuery.of(context).size.width * 0.05),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    "صلاة ${state.nextPrayerName}",
-                    style: const TextStyle(
-                      fontSize: 20,
-                      color: AppColors.kSecondaryColor,
-                    ),
-                  ),
-                  Text(
-                    state.nextPraying,
-                    style: const TextStyle(
-                      fontSize: 17,
-                      color: AppColors.kSecondaryColor,
-                    ),
-                    overflow: TextOverflow.ellipsis,
-                  ),
-                ],
-              ),
-            ],
-          ),
+Widget buildPrayerDetails(
+    BuildContext context, PrayingLoaded state, scrollController) {
+  return Column(
+    children: [
+      Container(
+        width: MediaQuery.of(context).size.width * 0.85,
+        height: MediaQuery.of(context).size.height * 0.12,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(12),
+          color: Colors.white,
         ),
-        SizedBox(height: MediaQuery.of(context).size.height * 0.02),
-        SingleChildScrollView(
+        padding: const EdgeInsets.all(10),
+        child: Row(
+          children: [
+            Image.asset(
+              "assets/images/next_prayer.png",
+              height: 50,
+            ),
+            SizedBox(width: MediaQuery.of(context).size.width * 0.05),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "صلاة ${state.nextPrayerName}",
+                  style: const TextStyle(
+                    fontSize: 20,
+                    color: AppColors.kSecondaryColor,
+                  ),
+                ),
+                Text(
+                  state.nextPraying,
+                  style: const TextStyle(
+                    fontSize: 17,
+                    color: AppColors.kSecondaryColor,
+                  ),
+                  overflow: TextOverflow.ellipsis,
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+      SizedBox(height: MediaQuery.of(context).size.height * 0.02),
+      Scrollbar(
+        controller: scrollController,
+        thumbVisibility: true,
+        child: SingleChildScrollView(
           scrollDirection: Axis.horizontal,
           controller: scrollController,
           child: Row(
@@ -54,7 +58,7 @@ Widget buildPrayerDetails(BuildContext context, PrayingLoaded state,scrollContro
             children: buildPrayerTimes(state),
           ),
         ),
-      ],
-    );
-  }
- 
+      ),
+    ],
+  );
+}
