cd323f4ed3e4b68c45815ac6d013fe1ea09b8a68
OmarEss3 <155445810+OmarEss3@users.noreply.github.com>
2025-01-20 17:08:49 +0200

    change doaa khatm& concatenate matching query in search page

--- lib/pages/quran_pages/search_page.dart
+++ lib/pages/quran_pages/search_page.dart
@@ -4,7 +4,6 @@ import 'package:azkar_app/pages/quran_pages/surah_page.dart';
 import 'package:azkar_app/utils/app_style.dart';
 import 'package:flutter/material.dart';
 import 'package:quran/quran.dart' as quran;
-// import 'package:quran/quran_text.dart';
 
 import '../../quran_text_no_diacritics.dart';
 
@@ -17,6 +16,8 @@ class SearchPage extends StatefulWidget {
 
 class SearchPageState extends State<SearchPage> {
   final FocusNode _searchFocusNode = FocusNode(); // FocusNode for the TextField
+  final TextEditingController _searchController = TextEditingController();
+  List<Map<String, dynamic>> searchResults = [];
 
   @override
   void initState() {
@@ -36,9 +37,6 @@ class SearchPageState extends State<SearchPage> {
     super.dispose();
   }
 
-  final TextEditingController _searchController = TextEditingController();
-  List<Map<String, dynamic>> searchResults = [];
-
   void _search(String query) {
     try {
       // Normalize the query
@@ -49,12 +47,10 @@ class SearchPageState extends State<SearchPage> {
       for (var verse in quranText) {
         String verseContent = normalizeArabic(verse['content']);
         if (verseContent.contains(processedQuery)) {
-          // Use contains here
           results.add({
             "surah": verse['surah_number'],
             "verse": verse['verse_number'],
-            "content": verse[
-                'content'], // Include the original content for highlighting
+            "content": verse['content'], // Include the original content
           });
         }
       }
@@ -84,26 +80,23 @@ class SearchPageState extends State<SearchPage> {
                 controller: _searchController,
                 focusNode: _searchFocusNode,
                 decoration: InputDecoration(
-                  focusColor: Colors.blueGrey, // Focus color for other parts
                   suffixIcon: Icon(
                     Icons.search,
                     color: AppStyles.styleRajdhaniBold20(context).color,
                   ),
                   labelText: "البحث",
-                  border: const OutlineInputBorder(), // Default border
+                  border: const OutlineInputBorder(),
                   focusedBorder: const OutlineInputBorder(
                     borderSide: BorderSide(
-                      color: Colors
-                          .blueGrey, // Blue-grey color for the focused state
-                      width: 1.0, // Optional: Adjust the border thickness
+                      color: Colors.blueGrey,
+                      width: 1.0,
                     ),
                   ),
                 ),
                 onChanged: _search,
               ),
             ),
-            if (searchResults
-                .isNotEmpty) // Show the count only when there are results
+            if (searchResults.isNotEmpty)
               Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 8.0),
                 child: Align(
@@ -119,32 +112,7 @@ class SearchPageState extends State<SearchPage> {
                 itemCount: searchResults.length,
                 itemBuilder: (context, index) {
                   final result = searchResults[index];
-                  String verseContent = result['content'];
-                  String normalizedContent = normalizeArabic(verseContent);
-                  String processedQuery =
-                      normalizeArabic(_searchController.text);
-
-                  List<TextSpan> spans = [];
-                  int startIndex = normalizedContent.indexOf(processedQuery);
-                  if (startIndex != -1) {
-                    spans.add(
-                        TextSpan(text: verseContent.substring(0, startIndex)));
-                    spans.add(TextSpan(
-                      text: verseContent.substring(
-                        startIndex,
-                        startIndex + processedQuery.length,
-                      ),
-                      style: const TextStyle(
-                          color: Colors.red, fontWeight: FontWeight.bold),
-                    ));
-                    spans.add(TextSpan(
-                        text: verseContent
-                            .substring(startIndex + processedQuery.length)));
-                  } else {
-                    spans.add(TextSpan(text: verseContent));
-                  }
-
-                  return SearchItem(spans: spans, result: result);
+                  return _buildSearchItem(result);
                 },
               ),
             ),
@@ -153,20 +121,15 @@ class SearchPageState extends State<SearchPage> {
       ),
     );
   }
-}
 
-class SearchItem extends StatelessWidget {
-  const SearchItem({
-    super.key,
-    required this.spans,
-    required this.result,
-  });
+  Widget _buildSearchItem(Map<String, dynamic> result) {
+    final verseContent = result['content'];
+    final processedQuery = normalizeArabic(_searchController.text);
+    final normalizedContent = normalizeArabic(verseContent);
 
-  final List<TextSpan> spans;
-  final Map<String, dynamic> result;
+    final spans =
+        _highlightQuery(normalizedContent, processedQuery, verseContent);
 
-  @override
-  Widget build(BuildContext context) {
     return ListTile(
       title: RichText(
         text: TextSpan(
@@ -176,6 +139,7 @@ class SearchItem extends StatelessWidget {
       ),
       subtitle: Text(
         'سورة: ${quran.getSurahNameArabic(result['surah'])}, آية: ${result['verse']}',
+        style: AppStyles.styleCairoMedium15white(context),
       ),
       onTap: () {
         Navigator.of(context).push(MaterialPageRoute(
@@ -188,5 +152,34 @@ class SearchItem extends StatelessWidget {
         ));
       },
     );
+  }
+
+  List<TextSpan> _highlightQuery(
+      String normalizedContent, String processedQuery, String originalContent) {
+    List<TextSpan> spans = [];
+    int startIndex = normalizedContent.indexOf(processedQuery);
+
+    while (startIndex != -1) {
+      spans.add(TextSpan(
+          text: originalContent.substring(0, startIndex),
+          style: AppStyles.styleUthmanicMedium30(context)));
+      spans.add(TextSpan(
+        text: originalContent.substring(
+            startIndex, startIndex + processedQuery.length),
+        style: AppStyles.styleUthmanicMedium30(context)
+            .copyWith(color: Colors.red),
+      ));
+      originalContent =
+          originalContent.substring(startIndex + processedQuery.length);
+      normalizedContent =
+          normalizedContent.substring(startIndex + processedQuery.length);
+      startIndex = normalizedContent.indexOf(processedQuery);
+    }
+
+    spans.add(TextSpan(
+        text: originalContent,
+        style: AppStyles.styleUthmanicMedium30(context)));
+
+    return spans;
   }
 }
